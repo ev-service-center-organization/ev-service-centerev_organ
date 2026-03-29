d04dddfbe9d117dacb26bbeea8662d62f0bac1ee
Chopper-97 <khangle0414@gmail.com>
2026-03-30 00:41:54 +0700

    update booking service

--- ev-service-center-backend/booking-service/src/app.js
+++ ev-service-center-backend/booking-service/src/app.js
@@ -11,11 +11,19 @@ app.use('/api/service-center', serviceCenterRoutes);
 app.get('/', (req, res) => res.send('📅 Booking Service is running'));
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
+const PORT = process.env.PORT || 5002; // Lấy port từ .env hoặc mặc định 5002
+
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
+    // await sequelize.sync(); // Mở dòng này nếu bạn muốn tự động tạo bảng
     console.log('✅ Database connected for Booking Service.');
+
+    // THÊM ĐOẠN NÀY VÀO:
+    app.listen(PORT, () => {
+      console.log(`🚀 Booking Service running on port ${PORT}`);
+    });
+
   } catch (err) {
     console.error('❌ Database connection failed:', err);
   }
--- ev-service-center-backend/booking-service/src/config/db.js
+++ ev-service-center-backend/booking-service/src/config/db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: 'ev_mysql',
+    host: '127.0.0.1',
     port: 3306,
     dialect: 'mysql',
     logging: false,
